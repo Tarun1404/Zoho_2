7e36ef9330f749946c3db27804978a0ea7010472
Tarun1404 <36921333+Tarun1404@users.noreply.github.com>
2018-04-03 00:49:55 +0530

    Update longestuniquesubset.c

--- longestuniquesubset.c
+++ longestuniquesubset.c
@@ -11,27 +11,38 @@ int main()
 
 void longsubstring(char* str)
 {
-    int n,i,j,k=0,l=0;
+    int n,i,j,k=0,l=0,f=0;
     n= strlen(str);
     char tempstr[100],reqstr[100] ="\0",test;
     
     for(i=0 ; i<n-1 ; i++)
     {
         tempstr[k]=str[i];
-    //    for(j=i+1 ; j<n ; j++)
-      //  {
-            for(l=0;l<k;l++)
+ 
+       for(j=i+1 ; j<n ; j++)
+        {
+        
+            for(l=0;l<=k;l++)
             {
-                
-            
-            if(str[i]==tempstr[l])
-            {
-                break;
+        
+                if(str[j]==tempstr[l])
+                {
+                    f=1;
+                    j=n;
+                    break;
+                }
             }
-        //    }
-            tempstr[++k]=str[i];
+        
+            if(f==0)
+            {
+                k++;                
+                tempstr[k]=str[j];
+            }
+                f=0;
         }
+   
         k=0;
+        
         if(strlen(tempstr)>strlen(reqstr))
         {
             strcpy (reqstr,tempstr);
